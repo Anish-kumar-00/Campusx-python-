cec31907670b61e4cb4475d2dc18512814e2804f
Anish Kumar <ay6704197@gmail.com>
2026-05-29 19:38:44 +0530

    update 24-7.py

--- 24-7.py
+++ 24-7.py
@@ -4,7 +4,10 @@ matrix = [ [1,2,3], [4,5,6], [7,8,9]]
 [1, 4, 7] [2, 5, 8] [3, 6, 9]
 """
 matrix = [ [1,2,3], [4,5,6], [7,8,9]]
-for i in range (0,3,)
-     temp=
-     for l in range(0,3,)
-         
+x=[]
+for i in range (0,3,):
+     temp=[]
+     for l in range(0,3,):
+         temp.append(matrix[l][i])
+     x.append(temp)
+print(x)     
