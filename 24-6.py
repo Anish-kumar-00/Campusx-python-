b83ff3d0cf0329fb2b65d4ea965e0cc7f3be3b59
Anish Kumar <ay6704197@gmail.com>
2026-05-29 19:59:23 +0530

    update 24-6.py

---
+++ 24-6.py
@@ -0,0 +1,17 @@
+"""
+###Problem 6: Find list of common unique items from two list. and show in increasing order
+Input
+num1 = [23,45,67,78,89,34]
+num2 = [34,89,55,56,39,67]
+Output:
+[34, 67, 89]
+"""
+num1 = [23,45,67,78,89,34]
+num2 = [34,89,55,56,39,67]
+num3=[]
+for i in num1:
+    for j in num2:
+        if(i==j):
+          num3.append(i)
+print(sorted(num3))          
+          
