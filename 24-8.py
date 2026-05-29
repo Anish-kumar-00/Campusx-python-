5338048adefb928fcb40c99548ad95ff49045d93
Anish Kumar <ay6704197@gmail.com>
2026-05-29 20:11:09 +0530

    update 24-8.py

---
+++ 24-8.py
@@ -0,0 +1,15 @@
+"""
+Problem 8: Split String of list on K character.
+Example :
+Input:
+['CampusX is a channel', 'for data-science', 'aspirants.']
+Output:
+['CampusX', 'is', 'a', 'channel', 'for', 'data-science', 'aspirants.']
+
+"""
+
+l=['CampusX is a channel', 'for data-science', 'aspirants.']
+l1=[]
+for i in l :
+    l1.extend(i[])
+print(l1)    
