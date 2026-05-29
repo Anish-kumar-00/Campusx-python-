5ab97406dc0e9e5dc09128994ba52d10f06e0d2a
Anish Kumar <ay6704197@gmail.com>
2026-05-29 20:25:22 +0530

    update 24-7.py

---
+++ 24-7.py
@@ -0,0 +1,6 @@
+###Problem 7: Sort a list of alphanumeric strings based on product value of numeric character
+in it. If in any string there is no numeric character take it’s product value as 1.
+Input:
+['1ac21', '23fg', '456', '098d','1','kls']
+Output:
+['456', '23fg', '1ac21', '1', 'kls', '098d']
