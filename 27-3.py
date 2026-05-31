792b92b4154a5fec01bf05b1a88486f7bb0afe65
Anish Kumar <ay6704197@gmail.com>
2026-05-31 08:30:29 +0530

    update 27-3.py

---
+++ 27-3.py
@@ -0,0 +1,10 @@
+"""
+###Q3: Check is tuples are same or not? Two tuples would be same if both tuples have same
+element at same index
+t1 = (1,2,3,0)
+t2 = (0,1,2,3)
+t1 and t2 are not same
+"""
+t1 = (1,2,3,0)
+t2 = (0,1,2,3)
+print(t1==t2)
