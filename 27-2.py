5a1584a754a136e62106bf15c6f4780e5f809c90
Anish Kumar <ay6704197@gmail.com>
2026-05-31 08:25:43 +0530

    update 27-2.py

---
+++ 27-2.py
@@ -0,0 +1,22 @@
+"""
+###Q2: Multiply Adjacent elements (both side) and take sum of right and lest side multiplication
+result.
+For eg.
+The original tuple : (1, 5, 7, 8, 10)
+Resultant tuple after multiplication :
+(1*5, 1*5+5*7, 7*5 + 7*8, 8*7 + 8*10, 10*8) -> (5, 40, 91, 136, 80)
+output-(5, 40, 91, 136, 80)
+
+"""
+l1=[1, 5, 7, 8, 10]
+
+l2=[]
+for i in l1:
+    k=l1.index(i)
+    if k>0 and k<(len(l1)-1):
+       l2.append(i*(l1[k-1]+l1[k+1]))
+    elif k==0:
+       l2.append(i*(l1[k+1]))
+    else:
+        l2.append(i*(l1[k-1]))
+print(l2)        
