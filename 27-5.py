07fa87320f2d083200a082857d70faeba0e14092
Anish Kumar <ay6704197@gmail.com>
2026-05-31 08:55:18 +0530

    update 27-5.py

---
+++ 27-5.py
@@ -0,0 +1,31 @@
+###Q5: Shortlist Students for a Job role Ask user to input students record and store in tuples for
+each record. Then Ask user to input three things he wants in the candidate- Primary Skill, Higher
+Education, Year of Graduation.
+Show every students record in form of tuples if matches all required criteria.
+It is assumed that there will be only one primry skill.
+If no such candidate found, print No such candidate
+Input:
+Enter No of records- 2
+Enter Details of student-1
+Enter Student name- Manohar
+Enter Higher Education- B.Tech
+Enter Primary Skill- Python
+Enter Year of Graduation- 2022
+Enter Details of student-2
+Enter Student name- Ponian
+Enter Higher Education- B.Sc.
+Enter Primary Skill- C++
+Enter Year of Graduation- 2020
+Enter Job Role Requirement
+Enter Skill- Python
+Enter Higher Education- B.Tech
+Enter Year of Graduation- 2022
+Output
+('Manohar', 'B.tech', 'Python', '2022')
+
+i=int(input(Enter no of records:-))
+l=[]
+for k in  range(i):
+     print(Enter Details of student)
+
+
