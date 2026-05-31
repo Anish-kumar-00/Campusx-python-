fe570311e4a004d87a96ca4e19be728d7090b9c9
Anish Kumar <ay6704197@gmail.com>
2026-05-31 08:54:03 +0530

    update 27-4.py

---
+++ 27-4.py
@@ -0,0 +1,10 @@
+"""
+###Q4: Count no of tuples, list and set from a list
+list1 = [{'hi', 'bye'},{'Geeks', 'forGeeks'},('a', 'b'),['hi', 'bye'],['a', 'b']]
+Output:
+    List-2
+Set-2
+Tuples-1
+"""
+
+print(isdigit())
