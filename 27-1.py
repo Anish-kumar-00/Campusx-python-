42fa7f6939bac2d1dc74fbd1ab273b16609ac768
Anish Kumar <ay6704197@gmail.com>
2026-05-31 06:35:35 +0530

    update 27-1.py

---
+++ 27-1.py
@@ -0,0 +1,15 @@
+
+"""
+###Q1: Join Tuples if similar initial element While working with Python tuples, we can have a
+problem in which we need to perform concatenation of records from the similarity of initial element.
+This problem can have applications in data domains such as Data Science.
+For eg.
+Input : test_list = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)]
+Output : [(5, 6, 7, 8), (6, 10), (7, 13)]
+"""
+
+
+l= [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)]
+l1=[]
+for i in l:
+    for j 
